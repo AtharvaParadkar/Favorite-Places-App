54bb1dfd2c3ab1a55e75861f09a5a5bddf321e5d
AtharvaParadkar <atharvaparadkar91@gmail.com>
2025-01-16 23:24:07 +0530

    Add location picking functionality to MapScreen

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -21,6 +21,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +37,11 @@ class _MapScreenState extends State<MapScreen> {
         ],
       ),
       body: GoogleMap(
+        onTap: (position) {
+          setState(() {
+            _pickedLocation = position;
+          });
+        },
         initialCameraPosition: CameraPosition(
           target: LatLng(
             widget.location.latitude,
@@ -42,12 +49,20 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 15,
         ),
-        markers: {
-          Marker(
-            markerId: const MarkerId('M1'),
-            position: LatLng(widget.location.latitude, widget.location.longitude)
-          ),
-        },
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('M1'),
+                  //~ '??' is a null-aware operator
+                  position:
+                      _pickedLocation ??   //? Uses _pickedLocation if it is not null otherwise uses LatLng()
+                          LatLng(
+                            widget.location.latitude,
+                            widget.location.longitude,
+                          ),
+                ),
+              },
       ),
     );
   }
