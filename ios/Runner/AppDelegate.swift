937ebdc29c6047c5562a9b0d4e767ece8e9632f4
AtharvaParadkar <atharvaparadkar91@gmail.com>
2025-01-16 22:17:00 +0530

    Update Google Maps API key in AppDelegate for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyBtQjgwKVugnzjsZ9XXz3oMGcjiGXPoXIw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
